a4414f2cd54655d0da3bdf0bc090c546a8e9eed5
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2021-02-22 19:52:05 -0300

    sobreescrevendo layout de registro do devise e permitindo o parametro name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
-    # layout :layout_by_resource
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  #def layout_by_resource
-   # if devise_controller? && resource_name == :registrations && action_name == 'sign_up'
-    #  "devise"
-    #else
-     # "application"
-    #end
-  #end
+  protected
+ 
+  def configure_permitted_parameters
+    attributes = [:name]
+     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+  end
 end
